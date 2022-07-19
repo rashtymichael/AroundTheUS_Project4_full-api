74f0dc1622050a08d4aba67ebe6240c7c3197d63
Micky Rashty <mrashty@gmail.com>
2022-07-20 01:46:11 +0300

    add winston logger
    3. Implementing logging of requests and errors

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const { unAuthRoutes, authRoutes } = require('./routes');
 const { mongoServer } = require('./utils');
 const auth = require('./middleware/auth');
 const centralErrorHandler = require('./middleware/errorHandler');
+const { requestLogger, errorLogger } = require('./middleware/Logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -14,9 +15,13 @@ mongoose.connect(mongoServer);
 
 app.use(bodyParser.json());
 
+app.use(requestLogger);
+
 app.use(unAuthRoutes);
 app.use(auth);
 app.use(authRoutes);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(centralErrorHandler);
